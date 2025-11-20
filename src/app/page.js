8131f72f58b43ca9ba190ca0f269ac262b18a8e4
junganthonyy <junganthonyy@gmail.com>
2025-11-19 16:19:31 -0800

    Threejs Tutorial Toroid

--- src/app/page.js
+++ src/app/page.js
@@ -1,65 +1,59 @@
-import Image from "next/image";
+"use client"
+import * as THREE from 'three';
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 
-export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+
+import { useEffect, useRef } from "react";
+
+export default function MyThree() {
+    const refContainer = useRef(null);
+
+    useEffect(() => {
+        // === THREE.JS CODE START ===
+        var scene = new THREE.Scene();
+        const fov = 45;
+        const aspect= window.innerWidth / window.innerHeight;
+        const near = 0.1;
+        const far = 100;
+
+        var camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+        var renderer = new THREE.WebGLRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        // document.body.appendChild( renderer.domElement );
+        // use ref as a mount point of the Three.js scene instead of the document.body
+        refContainer.current && refContainer.current.appendChild( renderer.domElement );
+
+
+        // var geometry = new THREE.BoxGeometry(1, 1, 1);
+        var geometry = new THREE.TorusGeometry(5, 2, 8, 6)
+
+        var material = new THREE.MeshBasicMaterial({
+            color: 0x00ffff,
+            wireframe: true ,
+        });
+        // var material = new THREE.MeshLambertMaterial({
+        //     color: 0xFF0000,
+        //     flatShading: true ,
+        // });
+
+        var volume = new THREE.Mesh(geometry, material);
+        scene.add(volume);
+        camera.position.z = 20;
+
+        const controls = new OrbitControls(camera, renderer.domElement);
+        controls.target.set(0, 5, 0);
+        controls.update();
+
+        var animate = function () {
+            requestAnimationFrame(animate);
+            // volume.rotation.x += 0.01;
+            // volume.rotation.y += 0.01;
+            renderer.render(scene, camera);
+        };
+        animate();
+    }, []);
+    return (
+        <div ref={refContainer}></div>
+
+    );
 }
