7dff01e304d1c16b9f44376b126dc45a1fd5af02
bryanherrera72 <bryan.herrera72@gmail.com>
2025-11-19 22:22:13 -0800

    Added froggy route + GLTFLoader import

---
+++ src/app/Froggy/page.js
@@ -0,0 +1,11 @@
+"use client";
+import * as THREE from "three";
+
+//will allow us to import blender GLB and GLTF models
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+
+import { useEffect, useRef } from "react";
+
+export default function Frog() {
+  return <div>page</div>;
+}
