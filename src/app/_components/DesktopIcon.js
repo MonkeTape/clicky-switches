9a066ecf272913c89c8f63bcccde1c1d9ce4f6ce
junganthonyy <junganthonyy@gmail.com>
2026-05-05 10:07:44 -0700

    Adding Draggable Fix

--- src/app/_components/DesktopIcon.js
+++ src/app/_components/DesktopIcon.js
@@ -1,22 +1,33 @@
 import Image from "next/image";
 import Draggable from "react-draggable";
-import {useRef} from 'react';
+import { useRef } from "react";
 
 function DesktopIcon({ imageSrc, alt, name }) {
-  const nodeRef = useRef(null);  
+  const nodeRef = useRef(null);
   // have a default image thing to use as the image if we don't get one
   const source = imageSrc ? imageSrc : "/default_icon.png";
   const altMessage = alt || `Icon for ${name}`;
 
-  const image = <Image src={source} width={50} height={50} alt={altMessage} />;
+  const image = (
+    <Image
+      src={source}
+      width={50}
+      height={50}
+      alt={altMessage}
+      draggable={false}
+    />
+  );
   return (
-    <Draggable nodeRef = {nodeRef}>
-    <div ref = {nodeRef} className="flex flex-col items-center justify-center text-black w-[50px] h-[75px]">
-      <div className="bg-computer-green-500">{image}</div>
-      <div className="bg-computer-green-500 text-ellipsis">
-        {name || "name"}
+    <Draggable nodeRef={nodeRef}>
+      <div
+        ref={nodeRef}
+        className="flex flex-col items-center justify-center text-black w-[50px] h-[75px]"
+      >
+        <div className="bg-computer-green-500">{image}</div>
+        <div className="bg-computer-green-500 text-ellipsis">
+          {name || "name"}
+        </div>
       </div>
-    </div>
     </Draggable>
   );
 }
