d395c5a7208fef41a15315a956130c8be9d1248d
Bryan H <bryan.herrera72@gmail.com>
2026-05-04 18:47:33 -0700

    adding draggable

--- src/app/_components/DesktopIcon.js
+++ src/app/_components/DesktopIcon.js
@@ -1,18 +1,23 @@
 import Image from "next/image";
+import Draggable from "react-draggable";
+import {useRef} from 'react';
 
 function DesktopIcon({ imageSrc, alt, name }) {
+  const nodeRef = useRef(null);  
   // have a default image thing to use as the image if we don't get one
   const source = imageSrc ? imageSrc : "/default_icon.png";
   const altMessage = alt || `Icon for ${name}`;
 
   const image = <Image src={source} width={50} height={50} alt={altMessage} />;
   return (
-    <div className="flex flex-col items-center justify-center text-black w-[50px] h-[75px]">
+    <Draggable nodeRef = {nodeRef}>
+    <div ref = {nodeRef} className="flex flex-col items-center justify-center text-black w-[50px] h-[75px]">
       <div className="bg-computer-green-500">{image}</div>
       <div className="bg-computer-green-500 text-ellipsis">
         {name || "name"}
       </div>
     </div>
+    </Draggable>
   );
 }
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,16 @@
-<<<<<<< HEAD
-import DesktopIcon from "./_components/DesktopIcon";
-
-export default function MyThree() {
-  return (
-    <div className="text-computer-green-500 plaid h-screen">
-      <div>Desktop Text</div>
-      <DesktopIcon name="tester" />
-    </div>
-=======
 "use client";
 import Draggable from "react-draggable";
 import {useRef} from 'react';
+
+import DesktopIcon from "./_components/DesktopIcon";
+
+
 export default function MyThree() {
   const nodeRef = useRef(null)
 
   return (
       <div className="text-computer-green-500">Desktop Text
-        <Draggable nodeRef={nodeRef}>
-          <div className= "w-10 bg-red-500" ref = {nodeRef}>I can now be moved around!</div>
-        </Draggable>
+        <DesktopIcon name = "anthonysicon"></DesktopIcon>
       </div>
->>>>>>> 09eebe1 (added draggable)
   );
 }
