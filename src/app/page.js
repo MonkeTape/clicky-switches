0f227e0e8d99d04fca5ce9155a07ed381336b602
junganthonyy <junganthonyy@gmail.com>
2026-05-27 17:06:16 -0700

    Adding Popup Window Updates

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,16 @@
-"use client";
-import Draggable from "react-draggable";
-import {useRef} from 'react';
-
 import DesktopIcon from "./_components/DesktopIcon";
-
+import FolderModal from "./_components/PopUpModals/FolderModal";
+import PopUpWindow from "./_components/PopUpModals/PopUpWindow";
 
 export default function MyThree() {
-  const nodeRef = useRef(null)
-
   return (
-      <div className="text-computer-green-500">Desktop Text
-        <DesktopIcon name = "anthonysicon"></DesktopIcon>
-      </div>
+    <div className="text-computer-green-500">
+      Desktop Text
+      <DesktopIcon name="anthonysicon"></DesktopIcon>
+      <PopUpWindow title={"Example!"}>
+        <div className="w-[400px]">Window Content</div>
+      </PopUpWindow>
+      <FolderModal />
+    </div>
   );
 }
