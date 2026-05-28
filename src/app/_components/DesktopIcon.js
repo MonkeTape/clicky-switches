d6750b872933f4d000baf86ff91a489c96d92176
junganthonyy <junganthonyy@gmail.com>
2026-05-27 17:05:26 -0700

    Updating Icon and Window

--- src/app/_components/DesktopIcon.js
+++ src/app/_components/DesktopIcon.js
@@ -1,8 +1,9 @@
+"use client";
 import Image from "next/image";
 import Draggable from "react-draggable";
 import { useRef } from "react";
 
-function DesktopIcon({ imageSrc, alt, name }) {
+function DesktopIcon({ imageSrc, alt, name, bounds = null }) {
   const nodeRef = useRef(null);
   // have a default image thing to use as the image if we don't get one
   const source = imageSrc ? imageSrc : "/default_icon.png";
@@ -15,13 +16,10 @@ function DesktopIcon({ imageSrc, alt, name }) {
       height={50}
       alt={altMessage}
       draggable={false}
-      style={{
-        maxWidth: "100%",
-        height: "auto"
-      }} />
+    />
   );
   return (
-    <Draggable nodeRef={nodeRef}>
+    <Draggable nodeRef={nodeRef} bounds={bounds}>
       <div
         ref={nodeRef}
         className="flex flex-col items-center justify-center text-black w-[50px] h-[75px]"
---
+++ src/app/_components/PopUpWindow.js
@@ -0,0 +1,5 @@
+function PopUpWindow() {
+  return <div></div>;
+}
+
+export default PopUpWindow;
