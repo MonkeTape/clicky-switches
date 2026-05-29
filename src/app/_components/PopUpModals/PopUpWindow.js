ed198cf72f63eb4f48d5d6d9d8e5d501b61c6f58
junganthonyy <junganthonyy@gmail.com>
2026-05-29 12:47:38 -0700

    Set Bounds, Adding Relative Class, Setting Sizes

--- src/app/_components/PopUpModals/PopUpWindow.js
+++ src/app/_components/PopUpModals/PopUpWindow.js
@@ -3,13 +3,18 @@ import Draggable from "react-draggable";
 import PopUpHeader from "./PopUpHeader";
 import { useRef } from "react";
 
-function PopUpWindow({ title, children }) {
+function PopUpWindow({ title, children, bounds = "parent" }) {
   const nodeRef = useRef(null);
   return (
-    <Draggable nodeRef={nodeRef} handle=".handle">
-      <div ref={nodeRef} className="fixed">
+    <Draggable
+      nodeRef={nodeRef}
+      handle=".handle"
+      className="w-50 h-50"
+      bounds={bounds}
+    >
+      <div ref={nodeRef} className="max-w-xl max-h-xl">
         <PopUpHeader>{title}</PopUpHeader>
-        <div className="bg-computer-green-500 border-2 border-t-0 border-black text-black">
+        <div className="bg-computer-green-500 border-2 border-t-0 border-black text-black p-5 relative">
           {children}
         </div>
       </div>
