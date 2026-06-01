d16df0e59919d3b7c4467ad921e3e35ea90cabeb
junganthonyy <junganthonyy@gmail.com>
2026-05-31 22:06:40 -0700

    Using Popover in Header

--- src/app/_components/Header.js
+++ src/app/_components/Header.js
@@ -1,16 +1,9 @@
-import { FaApple } from "react-icons/fa";
+import HeaderMenu from "./HeaderMenu";
 
 function Header() {
   return (
-    <div className="headerbar flex w-screen justify-between px-5 py-1 bg-computer-green-500 text-black border-b-black border-b-4">
-      <div className="flex flex-row gap-5 ">
-        <div className="text-2xl -mt-0.5">
-          <FaApple />
-        </div>
-        <div>File</div>
-        <div>View</div>
-        <div>Special</div>
-      </div>
+    <div className="headerbar flex w-screen justify-between px-5 bg-computer-green-500 text-black border-b-black border-b-4 z-10">
+      <HeaderMenu></HeaderMenu>
       <div>Time</div>
     </div>
   );
